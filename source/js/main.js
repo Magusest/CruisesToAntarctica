1b1d7f7f5a4b7e6e22dab7cecc00be8286630998
Magusest <redikor49@gmail.com>
2023-04-11 16:37:54 +0300

    Improve desktop view

--- source/js/main.js
+++ source/js/main.js
@@ -14,6 +14,11 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  const button = document.querySelector('.burger');
+  button.addEventListener('click', () => {
+    console.log('hi');
+  });
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
