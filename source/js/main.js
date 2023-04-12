cdd0fc655c1e3fb1fb28c9c359113d8ba5db4cee
Magusest <redikor49@gmail.com>
2023-04-12 11:27:21 +0300

    Prepare PP and add mobileMenu module

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {openMobileMenu} from './modules/my-moduls/mobile-menu';
 
 // ---------------------------------
 
@@ -14,10 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
-  const button = document.querySelector('.burger');
-  button.addEventListener('click', () => {
-    console.log('hi');
-  });
+  openMobileMenu();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
---
+++ source/js/modules/my-moduls/mobile-menu.js
@@ -0,0 +1,53 @@
+import {FocusLock} from '../../utils/focus-lock';
+const mobileMenuScreen = document.querySelector('[data-mobile-menu-screen]');
+const mobileMenu = mobileMenuScreen.querySelector('[data-mobile-menu]');
+const mobileMenuLink = mobileMenuScreen.querySelector('[data-mobile-link]');
+const mobileMenuLinks = mobileMenuScreen.querySelectorAll('[data-mobile-link]');
+const closeButton = mobileMenuScreen.querySelector('[data-close-mobile-menu]');
+const openButton = document.querySelector('[data-burger]');
+const mainLogo = document.querySelector('[data-main-logo]');
+const focusLock = new FocusLock();
+
+const closeMenuHendler = () => {
+  mobileMenuScreen.style.display = 'none';
+  document.body.style.overflow = '';
+  mainLogo.style.opacity = '1';
+  mainLogo.style.translate = '0';
+  focusLock.unlock();
+};
+
+const unscrolHendler = (evt) => {
+  if (evt.target === mobileMenuScreen) {
+    closeMenuHendler();
+  }
+}
+
+const openButtonHendler = () => {
+  mainLogo.style.opacity = '0';
+  mainLogo.style.translate = '100px';
+  mobileMenuScreen.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+  closeButton.addEventListener('click', closeMenuHendler);
+  focusLock.lock('[data-mobile-menu]');
+  mobileMenuLink.focus();
+  mobileMenuScreen.addEventListener('click', unscrolHendler);
+  mobileMenuLinks.forEach((link) => {
+    link.addEventListener('click', closeMenuHendler);
+  });
+};
+
+const onDocumentKeyDown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    closeMenuHendler();
+  }
+};
+
+function openMobileMenu() {
+  openButton.addEventListener('click', openButtonHendler);
+  document.addEventListener('keydown', onDocumentKeyDown);
+}
+
+export {
+  openMobileMenu
+};
