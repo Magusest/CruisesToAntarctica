334d82ffd952b7faeca04e830cc6e55235b18160
Magusest <redikor49@gmail.com>
2023-04-13 18:02:25 +0300

    Change mobile-menu markup

--- source/js/modules/my-moduls/mobile-menu.js
+++ source/js/modules/my-moduls/mobile-menu.js
@@ -1,35 +1,37 @@
 import {FocusLock} from '../../utils/focus-lock';
-const mobileMenuScreen = document.querySelector('[data-mobile-menu-screen]');
-const mobileMenuLink = mobileMenuScreen.querySelector('[data-mobile-link]');
-const mobileMenuLinks = mobileMenuScreen.querySelectorAll('[data-mobile-link]');
-const closeButton = mobileMenuScreen.querySelector('[data-close-mobile-menu]');
+const wrapper = document.querySelector('.wrapper');
+const mobileMenu = document.querySelector('[data-mobile-menu]');
+const mobileMenuList = document.querySelector('[data-mobile-menu-list]');
+const mobileMenuLink = document.querySelector('[data-mobile-link]');
+const mobileMenuLinks = document.querySelectorAll('[data-mobile-link]');
+const closeButton = document.querySelector('[data-close-mobile-menu]');
 const openButton = document.querySelector('[data-burger]');
 const mainLogo = document.querySelector('[data-main-logo]');
+const navLogo = document.querySelector('[data-nav-logo]');
 const focusLock = new FocusLock();
 
 const closeMenuHendler = () => {
-  mobileMenuScreen.style.display = 'none';
   document.body.style.overflow = '';
-  mainLogo.style.opacity = '1';
   mainLogo.style.translate = '0';
+  mainLogo.style.opacity = '1';
+  navLogo.style.display = 'none';
+  mobileMenu.classList.remove('nav--active');
+  mobileMenuList.classList.remove('nav__list--active');
+  wrapper.classList.remove('wrapper--active');
   focusLock.unlock();
 };
 
-const unscrolHendler = (evt) => {
-  if (evt.target === mobileMenuScreen) {
-    closeMenuHendler();
-  }
-};
-
 const openButtonHendler = () => {
+  mainLogo.style.translate = '36px';
   mainLogo.style.opacity = '0';
-  mainLogo.style.translate = '100px';
-  mobileMenuScreen.style.display = 'block';
+  navLogo.style.display = 'block';
+  mobileMenu.classList.add('nav--active');
+  mobileMenuList.classList.add('nav__list--active');
   document.body.style.overflow = 'hidden';
   closeButton.addEventListener('click', closeMenuHendler);
   focusLock.lock('[data-mobile-menu]');
   mobileMenuLink.focus();
-  mobileMenuScreen.addEventListener('click', unscrolHendler);
+  wrapper.classList.add('wrapper--active');
   mobileMenuLinks.forEach((link) => {
     link.addEventListener('click', closeMenuHendler);
   });
