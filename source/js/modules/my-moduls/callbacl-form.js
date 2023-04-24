bbd55a78c29493e49d7df75f56609231594c5bd0
Magusest <redikor49@gmail.com>
2023-04-24 17:34:49 +0300

    Fix bugs after second testing

--- source/js/modules/my-moduls/callbacl-form.js
+++ source/js/modules/my-moduls/callbacl-form.js
@@ -3,37 +3,44 @@ import {Form} from '../form-validate/form';
 const formValidator = new Form();
 const form = document.querySelector('[data-form]');
 const modal = document.querySelector('[data-modal="success"]');
+const error = document.querySelector('[data-modal="error"]');
 const phoneInput = document.querySelector('[data-phone-input]');
 
 const clickHendler = () => {
   modal.classList.remove('is-active');
+  error.classList.remove('is-active');
 };
 
 const onDocumentKeyDown = (evt) => {
   if (evt.key === 'Escape') {
     evt.preventDefault();
     modal.classList.remove('is-active');
+    error.classList.remove('is-active');
   }
 };
 
-const modalHendler = () => {
+const modalHandler = () => {
   document.addEventListener('click', clickHendler);
   document.addEventListener('keydown', onDocumentKeyDown);
 };
 
-// phoneInput.addEventListener('change', () => {
-//   console.log(phoneInput.querySelector('input').value.length);
-// });
+const errorHandler = () => {
+  document.addEventListener('click', clickHendler);
+  document.addEventListener('keydown', onDocumentKeyDown);
+};
 
 const submitHednler = (evt) => {
   if (phoneInput.querySelector('input').value.length === 18) {
     evt.preventDefault();
     form.reset();
     modal.classList.add('is-active');
-    modalHendler();
+    modalHandler();
+  } else {
+    evt.preventDefault();
+    error.classList.add('is-active');
+    errorHandler();
+    phoneInput.querySelector('input').focus();
   }
-  evt.preventDefault();
-  phoneInput.querySelector('input').focus();
 };
 
 function validateForm() {
